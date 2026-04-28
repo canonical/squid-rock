f24ca98cc2a0f2698bf6050fd8d6c4c5b14dedba
Alejandro Santisteban <alex.santisteban@canonical.com>
2026-04-28 13:24:02 +0200

    feat: squid 7.2-26.04 rock implementation (#2)
    * feat: squid 7.2-26.04 rock implementation
    
    * test: cat the log file to prevent race condition
    
    * chore: add squid rock coowner ( + newline nits) (#3)
    
    * chore: update codeowners
    
    * nit: newline
    
    * nit: newline
    
    * nit: newline
    
    * nit: newline
    
    * Update CODEOWNERS
    
    Signed-off-by: Alejandro Santisteban <alex.santisteban@canonical.com>
    
    ---------
    
    Signed-off-by: Alejandro Santisteban <alex.santisteban@canonical.com>
    Co-authored-by: Alejandro Santisteban <alex.santisteban@canonical.com>
    
    * test: add timeout function when waiting for setup to go up
    
    * test: update assertion
    
    Co-authored by @lczyk
    
    * test: add clarification comment
    
    * fix: follow logs by name and add --retry argument
    
    * fix: run the entrypoint with exec
    
    * refactor: add TODO comment
    
    ---------
    
    Signed-off-by: Alejandro Santisteban <alex.santisteban@canonical.com>
    Co-authored-by: Marcin Konowalczyk <marcin.konowalczyk@canonical.com>

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Squid OCI image entrypoint
+
+# This entrypoint aims to forward the squid logs to stdout to assist users of
+# common container related tooling (e.g., kubernetes, docker-compose, etc) to
+# access the service logs.
+
+# Moreover, it invokes the squid binary, leaving all the desired parameters to
+# be provided by the "command" passed to the spawned container. If no command
+# is provided by the user, the default behavior (as per the service.squid.command
+# in the rockcraft.yaml) will be to use Ubuntu's default configuration [1] and run
+# squid with the "-NYC" options to mimic the behavior of the Ubuntu provided
+# systemd unit.
+
+# [1] The default configuration is changed in the rockcraft.yaml to allow local
+# network connections. See the rockcraft.yaml for further information.
+
+tail --follow=name --retry /var/log/squid/access.log 2>/dev/null &
+tail --follow=name --retry /var/log/squid/error.log 2>/dev/null &
+tail --follow=name --retry /var/log/squid/store.log 2>/dev/null &
+tail --follow=name --retry /var/log/squid/cache.log 2>/dev/null &
+
+# create missing cache directories and exit
+/usr/sbin/squid-gnutls -Nz
+exec /usr/sbin/squid-gnutls "$@"
